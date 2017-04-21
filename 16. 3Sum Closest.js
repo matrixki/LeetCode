392c78f431c7158dd29e3727b61a5855446e2658
Mike-macbook <minka1101@gmail.com>
2017-04-21 15:09:28 +0800

    20170421

--- 16. 3Sum Closest.js
+++ 16. 3Sum Closest.js
@@ -6,3 +6,46 @@ Given an array S of n integers, find three integers in S such that the sum is cl
     The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    var result = 0, temp = 0, minDistance = Math.pow(2,31)-1, tempDistance = 0;
+    
+    if( nums.length <=3 ){ 
+        return nums[0]+nums[1]+nums[2]; 
+    }
+    else{
+        // sort array
+        nums = nums.sort(function(a, b){return a-b});
+        // loop through all posible combinations
+        for( i=0;i<nums.length-2;i++){
+            j=i+1, k=nums.length-1;
+            while( j<k ){
+                temp = nums[i] + nums[j] + nums[k];
+                tempDistance = Math.abs( temp-target );
+                if( temp > target ){
+                    if( tempDistance < minDistance ){
+                        minDistance = tempDistance;
+                        result = temp;
+                    }
+                    k--;
+                }
+                else if( temp < target ){
+                    if( tempDistance < minDistance ){
+                        minDistance = tempDistance;
+                        result = temp;                        
+                    }
+                    j++;
+                }
+                else{
+                    // temp equal to target
+                    return temp;
+                }
+            }
+        }
+        return result;
+    }
+};
