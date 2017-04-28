078e0393c6ec64ccea353777287b39898a07f7f9
Mike-macbook <minka1101@gmail.com>
2017-04-28 13:48:38 +0800

    20170428

--- 557. Reverse Words in a String III.js
+++ 557. Reverse Words in a String III.js
@@ -7,3 +7,23 @@ Output: "s'teL ekat edoCteeL tsetnoc"
 Note: In the string, each word is separated by single space and there will not be any extra space in the string.
 */
 
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    var sArray = s.split(" "), result="";
+    for(var i=0;i<sArray.length;i++){
+        var cStr = sArray[i];
+        var pointer = 0;
+        while(pointer < cStr.length){
+            result += cStr[cStr.length-1-pointer];
+            pointer++;
+        }
+        
+        if( i < (sArray.length-1) ){
+            result += ' ';
+        }
+    }
+    return result;
+};
