59f77f76bcbb8bfb06fa6e0fe71ed19a67f0a64e
Mike Ko <minka1101@gmail.com>
2017-04-29 23:27:48 +0800

    20170429

---
+++ 344. Reverse String.js
@@ -0,0 +1,19 @@
+/*
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) {
+    var pointer = 0, result='';
+    while(pointer<s.length){
+        result += s[s.length-1-pointer];
+        pointer++;
+    }
+    return result;
+};
