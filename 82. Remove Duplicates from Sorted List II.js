7d37196996b24886d3aa5c220371175b4f357d69
Mike Ko <minka1101@gmail.com>
2017-05-03 00:31:00 +0800

    20170502

--- 82. Remove Duplicates from Sorted List II.js
+++ 82. Remove Duplicates from Sorted List II.js
@@ -6,3 +6,40 @@ Given 1->2->3->3->4->4->5, return 1->2->5.
 Given 1->1->1->2->3, return 2->3.
 */
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(!head){ return head; }
+    var prev = new ListNode();
+    var node = new ListNode();
+    var current = new ListNode();
+    prev.next = head;
+    node = prev;
+    current = head;
+    var rmFlag = false;
+    while( current !== null ){
+        if( current.next && current.val === current.next.val ){
+            current.next = current.next.next;
+            rmFlag = true;
+        }
+        else if(rmFlag){
+            prev.next = current.next;
+            current = prev.next;
+            rmFlag = false;
+        }
+        else{
+            prev = current;
+            current = current.next;
+        }
+    }
+    return node.next;
+};
