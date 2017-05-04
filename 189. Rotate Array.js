d358c21e9904f6090defdc0141370323be26dba4
Mike-macbook <minka1101@gmail.com>
2017-05-04 16:58:59 +0800

    20170504

--- 189. Rotate Array.js
+++ 189. Rotate Array.js
@@ -13,3 +13,23 @@ Could you do it in-place with O(1) extra space?
 Related problem: Reverse Words in a String II
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    var step = k%nums.length;
+    var temp = [];
+    for(var i = nums.length-step; i<nums.length;i++){
+        temp.push( nums[i] );
+    }
+    for(i=0;i<nums.length;i++){
+        if( nums.length-i > step ){
+            nums[ nums.length-1-i ] = nums[ nums.length-1-i-step ]  
+        }
+        else{
+            nums[nums.length-1-i] = temp[nums.length-1-i];
+        }
+    }
+};
