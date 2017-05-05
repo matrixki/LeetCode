9a162aa7a8d89e50cb190e650043e286c250140e
Mike-macbook <minka1101@gmail.com>
2017-05-05 15:12:52 +0800

    20170505

--- 186. Reverse Words in a String II.js
+++ 186. Reverse Words in a String II.js
@@ -9,3 +9,31 @@ return "blue is sky the".
 
 Could you do it in-place without allocating extra space?
 */
+
+/**
+ * @param {character[]} str
+ * @return {void} Do not return anything, modify the string in-place instead.
+ */
+var reverseWords = function(str) {
+    
+    reverse(str, 0, str.length-1);
+    var low = 0;
+    for(var i=0;i<=str.length;i++){
+        if( str[i] === ' ' || i === str.length ){
+            console.log(low+' '+i);
+            if( low !== i-1 ){
+                reverse(str, low, i-1 );   
+            }
+            low = i+1;
+        }
+    }
+    
+    // function for reverse character array
+    function reverse(s, start, end){
+        for(var i=0;i<Math.floor( (end-start)/2)+1;i++){
+            var temp = s[start+i];
+            s[start+i] = s[end-i];
+            s[end-i] = temp;
+        }
+    }
+};
