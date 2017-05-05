b7e39f8124baf69adc05dbcbce43d6bc9b36bf69
Mike-macbook <minka1101@gmail.com>
2017-05-05 17:11:34 +0800

    20170505

--- 151. Reverse Words in a String.js
+++ 151. Reverse Words in a String.js
@@ -8,3 +8,35 @@ return "blue is sky the".
 Update (2015-02-12):
 For C programmers: Try to solve it in-place in O(1) space.
 */
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+var reverseWords = function(str) {
+    var sArray = str.trim().split(' ');
+    str = '';
+    for(var i=0;i<sArray.length;i++){
+        if(sArray[ sArray.length -1 -i ] !== ' ' && sArray[ sArray.length -1 -i ] !== ''){
+            str += sArray[ sArray.length -1 -i ]+' ';    
+        }
+    }
+    return str.trim();
+};
+
+/* better solution */
+var reverseWords = function(str) {
+    var strlen = str.length, result='';
+    for( var i=strlen-1;i>=0;i-- ){
+        if( str[i] ===' ' ){
+            strlen = i;
+        }    
+        else if( str[i-1]===' ' || i===0 ){
+            if( result !== '' ){
+                result += ' ';
+            }
+            result += str.substr(i,strlen-i);
+        }
+    }
+    return result;
+};
