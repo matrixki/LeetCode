023ce8a9af6c47b8707565345f50217f46a2c43a
Mike Ko <minka1101@gmail.com>
2017-05-09 00:28:21 +0800

    20170508

---
+++ 17. Letter Combinations of a Phone Number.js
@@ -0,0 +1,11 @@
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/#/description
+*/
+
