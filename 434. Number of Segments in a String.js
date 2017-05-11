05c8791fb78481f68c2abe91d58c3a8043df18dc
Mike-macbook <minka1101@gmail.com>
2017-05-11 13:06:26 +0800

    20170511

--- 434. Number of Segments in a String.js
+++ 434. Number of Segments in a String.js
@@ -9,3 +9,26 @@ Input: "Hello, my name is John"
 Output: 5
 */
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+    var count =0, flag=false;
+    // remove leading, trailing space
+    s = s.trim();
+    for(var i=0;i<s.length;i++){
+    	// condition: all spaces
+        if( s[i] !== ' ' ){
+            flag = true;
+        }
+        if( s[i] !== ' ' && s[i+1] === ' ' ){
+            count++;
+        }
+        // reach the end
+        if( i=== (s.length-1) && i > 1 ){
+            break;
+        }        
+    }
+    return flag?(count+1):0;
+};
