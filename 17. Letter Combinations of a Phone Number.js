afdf695b544bf98e74450ed03241fd484c59edae
Mike-macbook <minka1101@gmail.com>
2017-05-17 12:31:16 +0800

    20170517

--- 17. Letter Combinations of a Phone Number.js
+++ 17. Letter Combinations of a Phone Number.js
@@ -9,3 +9,32 @@ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 https://leetcode.com/problems/letter-combinations-of-a-phone-number/#/description
 */
 
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    var lookup = [ [], [], ['a','b','c'], ['d','e','f'], ['g','h','i'], ['j','k','l'], ['m','n','o'], ['p','q','r','s'], ['t','u','v'], ['w','x','y','z'] ];
+    var result = [];
+    for(var i=0;i<digits.length;i++){
+        var digit = digits[i];
+        var dict = lookup[digit];
+        if( result.length === 0 ){
+            for(var j=0;j<dict.length;j++){
+                result.push(dict[j]);
+            }
+        }
+        else{
+            var temp = [];
+            while(result.length >0){
+                var str = result.shift();
+                for(var k=0;k<dict.length;k++){
+                    var newStr = str+dict[k];
+                    temp.push(newStr);
+                }                
+            }
+            result = temp;
+        }
+    }
+    return result;
+};
