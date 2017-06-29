027703db4b2b504d3f444b302b3b5152d1a66664
Mike-macbook <minka1101@gmail.com>
2017-06-29 14:56:06 +0800

    20170629

--- 387. First Unique Character in a String.js
+++ 387. First Unique Character in a String.js
@@ -11,3 +11,22 @@ return 2.
 Note: You may assume the string contain only lowercase letters.
 */
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    var length = s.length;
+    var lookup = new Array(26);
+    lookup.fill(0);
+    for(var i=0; i<length ;i++){
+        lookup[ s.charCodeAt(i) - 97 ]++;
+    }
+    for(i=0;i<length;i++){
+        if( lookup[ s.charCodeAt(i) - 97 ] === 1 ){
+            return i;
+        }
+    }
+    
+    return -1;
+};
