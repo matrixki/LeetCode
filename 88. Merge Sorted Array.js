fafb1eb3b1892dd632102b50bafa310a356d8691
Mike Ko <minka1101@gmail.com>
2017-08-27 00:20:30 +0800

    20170827

---
+++ 88. Merge Sorted Array.js
@@ -0,0 +1,27 @@
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let pointer = m+n;
+    m--;
+    n--;
+    while(pointer--){
+        if( n<0 || nums1[m] > nums2[n] ){
+            nums1[pointer] = nums1[m--];
+        }       
+        else{
+            nums1[pointer] = nums2[n--];
+        }
+    }
+};
