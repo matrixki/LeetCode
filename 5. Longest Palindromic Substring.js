0a21ac82f36ca482a67f14cf5d17ea350b4a3418
matrixki <minka1101@gmail.com>
2017-08-29 13:02:49 +0800

    20170829

--- 5. Longest Palindromic Substring.js
+++ 5. Longest Palindromic Substring.js
@@ -15,6 +15,46 @@ Input: "cbbd"
 Output: "bb"
 */
 
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// update 20170829
+var longestPalindrome = function(s) {
+
+    let len = s.length;
+    if(len<=1){
+       return s;
+    }
+    else if(len===2 && s[0]===s[1]){
+        return s;
+    }
+    
+    let result = '';
+    for(let i=0;i<len;i++){
+        let temp1 = palinDromeCircle(s,i,i);
+        let temp2 = palinDromeCircle(s,i,i+1);
+        let temp = (temp1.right-temp1.left)>(temp2.right-temp2.left)?temp1:temp2; 
+        if( (temp.right-temp.left+1) > result.length ){
+            result = s.substring(temp.left, temp.right+1);
+        }
+    }
+    return result;
+};
+
+function palinDromeCircle(s, left, right){
+    while( s[left] === s[right] && left >=0 && right < s.length ){
+        left--;
+        right++;
+    }
+    return {
+        left: left+1,
+        right: right-1
+    };
+}
+
+
 /**
  * @param {string} s
  * @return {string}
