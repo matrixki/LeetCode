cd2603755aa38e604e86b21bbf356d8d1230bc68
Mike Ko <minka1101@gmail.com>
2017-09-28 23:54:39 +0800

    20170928

---
+++ 121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,30 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+*/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var localMin = prices[0];
+    var result = 0;
+    for(var i=1;i<prices.length;i++){
+        result = Math.max( prices[i] - localMin, result );
+        localMin = Math.min( prices[i], localMin );
+    }
+    return result;
+};
