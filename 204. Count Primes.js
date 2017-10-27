eb7a527e8435ec13e5d60a2979e3a5d6d8e6fb47
Mike Ko <minka1101@gmail.com>
2017-10-27 23:45:42 +0800

    20171027

---
+++ 204. Count Primes.js
@@ -0,0 +1,27 @@
+/*
+Count the number of prime numbers less than a non-negative number, n.
+
+Credits:
+Special thanks to @mithmatt for adding this problem and creating all test cases.
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    if( n<3 ){ return 0; }
+
+    var count = 0;
+    var primeMap = new Array(n);
+    for( var i=2;i<n;i++){
+        if( primeMap[i] ){
+            continue;
+        }
+        count++;
+        for(var j=i;j<n;j=j+i){
+            primeMap[j] = true;
+        }
+    }
+    return count;
+};
