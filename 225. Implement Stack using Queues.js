2f0a27b2db63318e6dbce161f2d2e04f707630d6
Mike Ko <minka1101@gmail.com>
2017-11-06 23:33:22 +0800

    20171106

---
+++ 225. Implement Stack using Queues.js
@@ -0,0 +1,61 @@
+/*
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Notes:
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+*/
+
+/**
+ * Initialize your data structure here.
+ */
+var MyStack = function() {
+    this.nums = [];
+};
+
+/**
+ * Push element x onto stack. 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.nums.push(x);
+};
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.nums.pop();
+};
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.nums[this.nums.length-1];
+};
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.nums.length > 0 ? false : true;    
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = Object.create(MyStack).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
