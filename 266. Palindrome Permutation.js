287f2657f5fc716d6e4a60f254b74090213b1c30
Mike Ko <minka1101@gmail.com>
2017-12-02 23:47:22 +0800

    20171203

---
+++ 266. Palindrome Permutation.js
@@ -0,0 +1,31 @@
+/*
+Given a string, determine if a permutation of the string could form a palindrome.
+
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var canPermutePalindrome = function(s) {
+    var lookup = [];
+    for(var i=0, len = s.length;i<len;i++){
+        if( lookup[ s[i] ] ){
+            lookup[ s[i] ]++;
+        }
+        else{
+            lookup[ s[i] ] = 1;
+        }
+    }
+    var oddcount = 0;
+    for( var j in lookup ){
+        if( lookup[j]%2 === 1 ){
+            oddcount++;
+        }
+    }
+    return oddcount<2;
+};
+
+// tags: Google Bloomberg Uber 
