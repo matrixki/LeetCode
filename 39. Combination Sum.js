ddfdd96310822d1e4d22cec9b7c4d90d0b5d08eb
Mike Ko <minka1101@gmail.com>
2018-02-25 01:34:52 +0800

    20180224

---
+++ 39. Combination Sum.js
@@ -0,0 +1,46 @@
+/*
+Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+*/
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+  
+    candidates.sort(function(a, b) {
+        return a - b;
+    });
+    var result = [];
+    
+    dfs( 0, target, candidates, 0, [], result );
+    
+    function dfs(start, target, candidates, sum, curr, result){
+        if( sum === target ){
+            result.push(curr.concat());
+        }
+        for( var i=start, len = candidates.length;i<len;i++ ){
+            if( sum+candidates[i] <= target ){
+                curr.push(candidates[i]);
+                dfs( i, target, candidates, sum+candidates[i], curr, result );
+                curr.pop();
+            }
+        }
+    }
+    return result;
+};
+
+//tags: Uber, Snapchat
