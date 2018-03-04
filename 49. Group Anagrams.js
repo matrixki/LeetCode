638b6c14f94536baddd8f4a1db96b16e19645fb9
Mike Ko <minka1101@gmail.com>
2018-03-05 00:27:56 +0800

    20180305

---
+++ 49. Group Anagrams.js
@@ -0,0 +1,39 @@
+/*
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note: All inputs will be in lower-case.
+*/
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    var map = new Map();
+    var result = [];
+    for( str of strs ){
+        var sortedStr = str.split('').sort().join('');
+        if( map.has(sortedStr) ){
+            var temp = map.get(sortedStr);
+            temp.push(str);
+            map.set( sortedStr, temp );
+        }
+        else{
+            map.set( sortedStr, [ str ] );
+        }
+    }
+    for( var val of map.values() ){
+        result.push( val );
+    }
+    return result;
+};
+
+//tags: Facebook, Amazon, Bloomberg, Uber, Yelp
