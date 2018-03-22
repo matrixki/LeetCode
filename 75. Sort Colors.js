bbe96526ae992ad5da26e3ab491d7722926981c6
Mike Ko <minka1101@gmail.com>
2018-03-22 23:58:42 +0800

    20180321

---
+++ 75. Sort Colors.js
@@ -0,0 +1,44 @@
+/*
+Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note:
+You are not suppose to use the library's sort function for this problem.
+
+click to show follow up.
+
+Follow up:
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+
+Could you come up with an one-pass algorithm using only constant space?
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    var low = 0, high = nums.length-1, i=0;
+    while(low<high&&i<=high){
+        if( nums[i] === 0 ){
+            var temp = nums[i];
+            nums[i] = nums[low];
+            nums[low] = temp;
+            low++;
+            i++;
+        }
+        else if( nums[i] === 2 ){
+            var temp = nums[i];
+            nums[i] = nums[high];
+            nums[high] = temp;
+            high--;
+        }
+        else{
+            i++;
+        }
+    }
+};
+
+//tags: Facebook, Microsoft, Pocket Gems
