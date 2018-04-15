6672065d77838f738f62430dc16b67434617ab0f
Mike Ko <minka1101@gmail.com>
2018-04-16 00:38:35 +0800

    20180416

---
+++ 94. Binary Tree Inorder Traversal.js
@@ -0,0 +1,43 @@
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+For example:
+Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    var result = [];
+    var stack = [];
+    var node  = root;
+    while( node !== null || stack.length>0 ){
+        while(node!==null){
+            stack.push(node);
+            node = node.left;
+        }
+        node = stack.pop();
+        result.push(node.val);
+        node = node.right;
+    }
+    return result;
+};
+
+//tags: Microsoft
