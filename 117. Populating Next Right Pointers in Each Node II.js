5e4095e9208be4a8622563e2b30970fbd59be8dc
Mike Ko <minka1101@gmail.com>
2018-05-06 17:39:42 +0800

    20180506

---
+++ 117. Populating Next Right Pointers in Each Node II.js
@@ -0,0 +1,68 @@
+/*
+Given a binary tree
+
+struct TreeLinkNode {
+  TreeLinkNode *left;
+  TreeLinkNode *right;
+  TreeLinkNode *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+Example:
+
+Given the following binary tree,
+
+     1
+   /  \
+  2    3
+ / \    \
+4   5    7
+After calling your function, the tree should look like:
+
+     1 -> NULL
+   /  \
+  2 -> 3 -> NULL
+ / \    \
+4-> 5 -> 7 -> NULL
+*/
+
+/**
+ * Definition for binary tree with next pointer.
+ * function TreeLinkNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = this.next = null;
+ * }
+ */
+
+/**
+ * @param {TreeLinkNode} root
+ * @return {void} Do not return anything, modify tree in-place instead.
+ */
+var connect = function(root) {
+    var node = new TreeLinkNode();
+    var pre = node;
+    while(root){
+        if(root.left){
+            pre.next = root.left;
+            pre = pre.next;
+        }
+        if(root.right){
+            pre.next = root.right;
+            pre = pre.next;
+        }
+        root = root.next;
+        if(!root){
+            pre = node;
+            root = node.next;
+            node.next = null;
+        }
+    }    
+};
+
+//tags: Facebook, Microsoft, Bloomberg
