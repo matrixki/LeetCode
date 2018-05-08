4cae39c5622053e3808b6626b2ab771007ba2a9d
Mike Ko <minka1101@gmail.com>
2018-05-09 00:07:57 +0800

    20180509

---
+++ 129. Sum Root to Leaf Numbers.js
@@ -0,0 +1,65 @@
+/*
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    var result = 0;
+    if(!root){ return result; }
+    goDown(root, root.val);
+    return result;
+    
+    function goDown(node, curr){
+        if(!node.left && !node.right){
+            result += curr;
+        }
+        if(node.left){
+            goDown(node.left, curr*10+node.left.val);
+        }
+        if(node.right){
+            goDown(node.right, curr*10+node.right.val);
+        }
+    }
+};
