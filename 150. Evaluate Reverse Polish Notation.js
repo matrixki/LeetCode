cf70ed26fbe4839e0a590271f4ea2afeb8abfc4e
Mike Ko <minka1101@gmail.com>
2018-05-28 01:08:46 +0800

    20180527

---
+++ 150. Evaluate Reverse Polish Notation.js
@@ -0,0 +1,63 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Note:
+
+Division between two integers should truncate toward zero.
+The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+Example 1:
+
+Input: ["2", "1", "+", "3", "*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: ["4", "13", "5", "/", "+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+Output: 22
+Explanation: 
+  ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+*/
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    var stack = [];
+    for(var i=0, len=tokens.length;i<len;i++){
+        switch(tokens[i]){
+            case '+':
+                stack.push( stack.pop() + stack.pop() );
+                break;
+            case '-':
+                stack.push( -stack.pop() + stack.pop() );
+                break;
+            case '*':
+                stack.push( stack.pop() * stack.pop() );
+                break;
+            case '/':
+                var bottom = stack.pop();
+                var up = stack.pop();
+                stack.push( parseInt(up/bottom) );
+                break;
+            default: 
+                stack.push( parseInt(tokens[i]));
+        }
+    }
+    return stack.pop();
+};
+
+//tags: Linkedin
