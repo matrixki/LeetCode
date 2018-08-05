7e108e884fbb27ac0b46a76803d3d061a2fd805f
Mike Ko <minka1101@gmail.com>
2018-08-05 23:14:12 +0800

    20180805

---
+++ 238. Product of Array Except Self.js
@@ -0,0 +1,34 @@
+/*
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    var result = [];
+    var len = nums.length;
+    result[0] = 1;
+    for(var i=1;i<len;i++){
+        result[i] = result[i-1]*nums[i-1];
+    }
+    var right = 1;
+    for(var i=len-1;i>=0;i--){
+        result[i] = result[i]*right;
+        right *= nums[i];
+    }
+    return result;
+};
+
+//tags: Lyft, Facebook, Amazon, Apple, Hulu, Google, Zenefits, Expedia, Yelp, Microsoft, Linkedin
+
