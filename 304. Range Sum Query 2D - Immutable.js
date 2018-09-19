a8660e211da90b118864c907129058d3c898adda
Mike Ko <minka1101@gmail.com>
2018-09-20 02:00:33 +0800

    20180919

---
+++ 304. Range Sum Query 2D - Immutable.js
@@ -0,0 +1,64 @@
+/*
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+Note:
+You may assume that the matrix does not change.
+There are many calls to sumRegion function.
+You may assume that row1 ≤ row2 and col1 ≤ col2.
+*/
+
+/**
+ * @param {number[][]} matrix
+ */
+var NumMatrix = function(matrix) {
+    var row = matrix.length;
+    var col = row>0 ? matrix[0].length : 0;
+    var dp = [];
+    for(var i=0;i<row+1;i++){
+        dp[i] = [];
+        for(var j=0;j<col+1;j++){
+            dp[i][j] = 0;
+        }
+    }
+    for(var i=1;i<=row;i++){
+        for(var j=1;j<=col;j++){
+            dp[i][j] = matrix[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+        }
+    }   
+    this.dp = dp;
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
+    return this.dp[row2+1][col2+1] - this.dp[row1][col2+1] - this.dp[row2+1][col1] + this.dp[row1][col1];
+};
+
+/** 
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = Object.create(NumMatrix).createNew(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+
+ //tags: Facebook, Google, Microsoft, Houzz
+ 
