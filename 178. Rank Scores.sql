86abac34321a4024698c10e8c8b5151182d7990c
Mike-macbook <minka1101@gmail.com>
2018-10-19 00:47:32 -0700

    20181019

--- 178. Rank Scores.sql
+++ 178. Rank Scores.sql
@@ -27,3 +27,4 @@
 # Write your MySQL query statement below
 Select Score, (select count(distinct Score) from Scores where Score >= s.Score ) Rank from Scores s ORDER BY Score DESC;
 
+#tags: Alibaba, Tencent
