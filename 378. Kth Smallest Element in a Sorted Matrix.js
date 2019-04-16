34ece0472b7c1fafeff7edc1e7782f185fea9609
Mike Ko <mike.ko@workboard.com>
2019-04-16 12:08:04 -0700

    20190416

--- 378. Kth Smallest Element in a Sorted Matrix.js
+++ 378. Kth Smallest Element in a Sorted Matrix.js
@@ -16,3 +16,31 @@ return 13.
 Note: 
 You may assume k is always valid, 1 ≤ k ≤ n2.
 */
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+    let start = matrix[0][0], end = matrix[matrix.length-1][matrix[0].length-1];
+    while(start<end){
+        let mid = Math.floor((start+end)/2);
+        let count = 0, j = matrix[0].length -1;
+        for(let i=0;i<matrix.length;i++){
+            while(j>=0 && matrix[i][j]>mid){
+                j--;
+            }
+            count += (j+1);
+        }
+        if(count<k){
+            start = mid+1;
+        }
+        else{
+            end = mid;
+        }
+    }
+    return start;
+};
+
+//tags: Google, Facebook, Amazon, Adobe, Uber
