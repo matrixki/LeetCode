2e0762042343eba00e82c3cae10661a8da690579
Mike's Mac Mini <minka1101@gmail.com>
2021-07-30 23:23:55 -0700

    1758. Minimum Changes To Make Alternating Binary String

---
+++ 1758. Minimum Changes To Make Alternating Binary String.js
@@ -0,0 +1,55 @@
+/*
+
+You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0' to '1' or vice versa.
+
+The string is called alternating if no two adjacent characters are equal. For example, the string "010" is alternating, while the string "0100" is not.
+
+Return the minimum number of operations needed to make s alternating.
+
+ 
+
+Example 1:
+
+Input: s = "0100"
+Output: 1
+Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+Example 2:
+
+Input: s = "10"
+Output: 0
+Explanation: s is already alternating.
+Example 3:
+
+Input: s = "1111"
+Output: 2
+Explanation: You need two operations to reach "0101" or "1010".
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s[i] is either '0' or '1'.
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var minOperations = function(s) {
+    let result1 = 0, result2 = 0;
+    let combo1 = "0", combo2 = "1";
+    for(let i=0;i<s.length;i++){
+        //  start with 0
+        if(s[i]!==combo1){
+            result1++;
+        }
+        //  start with 1
+        if(s[i]!==combo2){
+            result2++;
+        }
+        combo1 = combo1 === "0" ? "1" : "0";
+        combo2 = combo2 === "0" ? "1" : "0";
+    }
+    return Math.min(result1, result2);
+};
