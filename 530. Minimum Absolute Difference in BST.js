3b1a117b6f16f2258a9a4aa939b93de47fc143a5
Mike's Mac Mini <minka1101@gmail.com>
2021-08-15 23:27:50 -0700

    530. Minimum Absolute Difference in BST

---
+++ 530. Minimum Absolute Difference in BST.js
@@ -0,0 +1,55 @@
+/*
+
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,6,1,3]
+Output: 1
+Example 2:
+
+
+Input: root = [1,0,48,null,null,12,49]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 104].
+0 <= Node.val <= 105
+ 
+
+Note: This question is the same as 783: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var getMinimumDifference = function(root) {
+    let result = 100000;
+    let prev = null;
+    
+    function dfs(root){
+        if(root === null){ return; }
+        dfs(root.left);
+        if(prev !== null){
+            result = Math.min(result, (root.val - prev.val));
+        }
+        prev = root;
+        dfs(root.right);
+    }
+    dfs(root);
+    return result;
+};
