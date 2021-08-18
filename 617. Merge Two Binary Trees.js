fc94f0a241e1c55737dc8adbe68e03442e40c0c1
Mike's Mac Mini <minka1101@gmail.com>
2021-08-18 15:51:05 -0700

    617. Merge Two Binary Trees

---
+++ 617. Merge Two Binary Trees.js
@@ -0,0 +1,56 @@
+/*
+
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+Example 2:
+
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 2000].
+-104 <= Node.val <= 104
+
+
+tags: Amazon, Facebook, Microsoft
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {TreeNode}
+ */
+ var mergeTrees = function(root1, root2) {
+    if(!root1 && !root2){ return null; }
+    if( !root1 || !root2 ) return root1 || root2;
+    
+    const nodeVal = ( root1 ? root1.val : 0 ) + ( root2 ? root2.val : 0 );
+    let newTree = new TreeNode(nodeVal);
+    newTree.left = mergeTrees(root1.left, root2.left);
+    newTree.right = mergeTrees(root1.right, root2.right);
+    return newTree;
+};
