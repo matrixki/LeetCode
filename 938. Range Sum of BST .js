12e15d4d98305d2bf4fba55b9e0d25b7bf3a0f1c
Mike's Mac Mini <minka1101@gmail.com>
2021-08-25 23:56:49 -0700

    938. Range Sum of BST

---
+++ 938. Range Sum of BST .js
@@ -0,0 +1,64 @@
+/*
+
+Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+Example 2:
+
+
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 2 * 104].
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+All Node.val are unique.
+
+tags: Facebook, Amazon
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+ var rangeSumBST = function(root, low, high) {
+    let result = 0;
+    function dfs(node){
+        if(!node){ return; }
+        if(node.val >= low && node.val <= high){
+            result += node.val;
+            dfs(node.left);
+            dfs(node.right);
+        }
+        else if(node.val < low){
+            dfs(node.right);
+        }
+        else if(node.val > high){
+            dfs(node.left);
+        }
+    }
+    dfs(root);
+    return result;
+};
