e2578481a5d7ccbb64907679439c43d75c3b5352
Mike's Mac Mini <minka1101@gmail.com>
2021-09-23 00:14:37 -0700

    1570. Dot Product of Two Sparse Vectors

---
+++ 1570. Dot Product of Two Sparse Vectors.js
@@ -0,0 +1,75 @@
+/*
+
+Given two sparse vectors, compute their dot product.
+
+Implement class SparseVector:
+
+SparseVector(nums) Initializes the object with the vector nums
+dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+A sparse vector is a vector that has mostly zero values, you should store the sparse vector efficiently and compute the dot product between two SparseVector.
+
+Follow up: What if only one of the vectors is sparse?
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+Output: 8
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+Example 2:
+
+Input: nums1 = [0,1,0,0,0], nums2 = [0,0,0,0,2]
+Output: 0
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 0*0 + 1*0 + 0*0 + 0*0 + 0*2 = 0
+Example 3:
+
+Input: nums1 = [0,1,0,0,2,0,0], nums2 = [1,0,0,0,3,0,4]
+Output: 6
+ 
+
+Constraints:
+
+n == nums1.length == nums2.length
+1 <= n <= 10^5
+0 <= nums1[i], nums2[i] <= 100
+
+tags: Facebook, Microsoft
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {SparseVector}
+ */
+ var SparseVector = function(nums) {
+    this.vector = new Map();
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==0){
+            this.vector.set(i, nums[i]);
+        }
+    }
+};
+
+// Return the dotProduct of two sparse vectors
+/**
+ * @param {SparseVector} vec
+ * @return {number}
+ */
+SparseVector.prototype.dotProduct = function(vec) {
+    let result = 0;
+    for(let [key, val] of vec.vector){
+        if(this.vector.has(key)){
+            result += this.vector.get(key)*val;
+        }
+    }
+
+    return result;
+};
+
+// Your SparseVector object will be instantiated and called as such:
+// let v1 = new SparseVector(nums1);
+// let v2 = new SparseVector(nums2);
+// let ans = v1.dotProduct(v2);
