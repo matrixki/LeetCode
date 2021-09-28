c9fde388c5c1ebb114773caf1a4bb0be368b6ff4
Mike's Mac Mini <minka1101@gmail.com>
2021-09-28 16:44:10 -0700

    1428. Leftmost Column with at Least a One

---
+++ 1428. Leftmost Column with at Least a One.js
@@ -0,0 +1,103 @@
+/*
+
+(This problem is an interactive problem.)
+
+A row-sorted binary matrix means that all elements are 0 or 1 and each row of the matrix is sorted in non-decreasing order.
+
+Given a row-sorted binary matrix binaryMatrix, return the index (0-indexed) of the leftmost column with a 1 in it. If such an index does not exist, return -1.
+
+You can't access the Binary Matrix directly. You may only access the matrix using a BinaryMatrix interface:
+
+BinaryMatrix.get(row, col) returns the element of the matrix at index (row, col) (0-indexed).
+BinaryMatrix.dimensions() returns the dimensions of the matrix as a list of 2 elements [rows, cols], which means the matrix is rows x cols.
+Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer. Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+For custom testing purposes, the input will be the entire binary matrix mat. You will not have access to the binary matrix directly.
+
+ 
+
+Example 1:
+
+
+
+Input: mat = [[0,0],[1,1]]
+Output: 0
+Example 2:
+
+
+
+Input: mat = [[0,0],[0,1]]
+Output: 1
+Example 3:
+
+
+
+Input: mat = [[0,0],[0,0]]
+Output: -1
+Example 4:
+
+
+
+Input: mat = [[0,0,0,1],[0,0,1,1],[0,1,1,1]]
+Output: 1
+ 
+
+Constraints:
+
+rows == mat.length
+cols == mat[i].length
+1 <= rows, cols <= 100
+mat[i][j] is either 0 or 1.
+mat[i] is sorted in non-decreasing order.
+
+tags: Facebook
+
+*/
+
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function BinaryMatrix() {
+ *     @param {integer} row, col
+ *     @return {integer}
+ *     this.get = function(row, col) {
+ *         ...
+ *     };
+ *
+ *     @return {[integer, integer]}
+ *     this.dimensions = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {BinaryMatrix} binaryMatrix
+ * @return {number}
+ */
+ var leftMostColumnWithOne = function(binaryMatrix) {
+    const [rows, cols] = binaryMatrix.dimensions();
+    let left = 0, right = cols-1;
+    let result = -1;
+    while(left<=right){
+        const mid = left + Math.floor((right-left)/2);
+        if(hasOneInColumn(binaryMatrix, rows, mid)){
+            result = mid;
+            right = mid-1;
+        }
+        else{
+            left = mid+1;
+        }
+    }
+    return result;
+    
+    function hasOneInColumn(matrix, m, n){
+        for(let i=0;i<m;i++){
+            if(matrix.get(i, n) === 1){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+};
