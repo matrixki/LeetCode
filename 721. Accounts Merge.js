38d00ec2bd46619f0a3abe78aa5d9651c6ee0185
Mike's Mac Mini <minka1101@gmail.com>
2021-10-03 23:49:19 -0700

    721. Accounts Merge

---
+++ 721. Accounts Merge.js
@@ -0,0 +1,90 @@
+/*
+
+Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+
+Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+
+After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+ 
+
+Example 1:
+
+Input: accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+Output: [["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+Explanation:
+The first and second John's are the same person as they have the common email "johnsmith@mail.com".
+The third John and Mary are different people as none of their email addresses are used by other accounts.
+We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'], 
+['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+Example 2:
+
+Input: accounts = [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+Output: [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+ 
+
+Constraints:
+
+1 <= accounts.length <= 1000
+2 <= accounts[i].length <= 10
+1 <= accounts[i][j] <= 30
+accounts[i][0] consists of English letters.
+accounts[i][j] (for j > 0) is a valid email.
+
+tags: Facebook, Google, Microsoft, Apple, Amazon, Bloomberg, Pinterest
+
+*/
+
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+ var accountsMerge = function(accounts) {
+    let graph = {}, emailToName = {}, result = [];
+    for(let i=0;i<accounts.length;i++){
+        const name = accounts[i][0];
+        const firstEmail = accounts[i][1];
+        for(let j=1;j<accounts[i].length;j++){
+            const email = accounts[i][j];
+            emailToName[email] = name;
+            addGraph(graph, firstEmail, email);
+            addGraph(graph, email, firstEmail);
+        }
+    }
+   
+
+    let visited = new Set();
+    for(let key of Object.keys(graph)){
+        let current = new Set();
+        if(!visited.has(key)){
+            dfs(graph, key, current, visited);
+        }
+        
+        if(current.size > 0){
+            let mergedEmail = [...current].sort();
+            const name = emailToName[mergedEmail[0]];
+            mergedEmail.unshift(name);
+            result.push(mergedEmail);
+        }
+    }
+    
+    return result;
+    
+    function dfs(graph, source, current, visited){
+        visited.add(source);
+        current.add(source);
+        for(let neighbor of graph[source]){
+            if(!current.has(neighbor)){
+                dfs(graph, neighbor, current, visited);
+            }
+        }
+    }
+    
+    
+    function addGraph(graph, from ,to){
+        if(from in graph === false){
+            graph[from] = [];
+        }
+        graph[from].push(to);
+    }
+};
