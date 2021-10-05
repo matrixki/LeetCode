1acee571258df2057108465e3227b9420a02a237
Mike's Mac Mini <minka1101@gmail.com>
2021-10-04 23:41:56 -0700

    543. Diameter of Binary Tree

---
+++ 543. Diameter of Binary Tree.js
@@ -0,0 +1,55 @@
+/*
+
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+
+tags: Facebook, Amazon, Microsoft, Google, Bloomberg, Apple, Tesla, Intuit
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var diameterOfBinaryTree = function(root) {
+    let result = 0;
+    traverse(root);
+    return result;
+    function traverse(node, length){
+        if(!node){ return 0; }
+        const left = traverse(node.left);
+        const right = traverse(node.right);
+        result = Math.max(result, left+right);
+        return Math.max(left, right)+1;
+    }
+};
