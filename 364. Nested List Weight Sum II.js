5b2703709d115367c3616d44788c52e1c81c1552
Mike's Mac Mini <minka1101@gmail.com>
2021-10-28 11:46:05 -0700

    364. Nested List Weight Sum II

---
+++ 364. Nested List Weight Sum II.js
@@ -0,0 +1,109 @@
+/*
+
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists.
+
+The depth of an integer is the number of lists that it is inside of. For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth. Let maxDepth be the maximum depth of any integer.
+
+The weight of an integer is maxDepth - (the depth of the integer) + 1.
+
+Return the sum of each integer in nestedList multiplied by its weight.
+
+ 
+
+Example 1:
+
+
+Input: nestedList = [[1,1],2,[1,1]]
+Output: 8
+Explanation: Four 1's with a weight of 1, one 2 with a weight of 2.
+1*1 + 1*1 + 2*2 + 1*1 + 1*1 = 8
+Example 2:
+
+
+Input: nestedList = [1,[4,[6]]]
+Output: 17
+Explanation: One 1 at depth 3, one 4 at depth 2, and one 6 at depth 1.
+1*3 + 4*2 + 6*1 = 17
+ 
+
+Constraints:
+
+1 <= nestedList.length <= 50
+The values of the integers in the nested list is in the range [-100, 100].
+The maximum depth of any integer is less than or equal to 50.
+
+tags: LinkedIn
+
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     @return {void}
+ *     this.setInteger = function(value) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     @return {void}
+ *     this.add = function(elem) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @param {NestedInteger[]} nestedList
+ * @return {number}
+ */
+ var depthSumInverse = function(nestedList) {
+    let result = 0;
+    helper(nestedList, calculateMaxDepth(nestedList));
+    
+    return result;
+    
+    function helper(nestedList, level){
+        for(let i=0;i<nestedList.length;i++){
+            const curr = nestedList[i];
+            if(curr.isInteger()){
+                result += curr.getInteger()*level;
+            }
+            else{
+                helper(curr.getList(), level-1);
+            }
+        }
+    }
+    
+    function calculateMaxDepth(nestedList){
+        let maxDepth = 0;
+        for(let i=0;i<nestedList.length;i++){
+            const curr = nestedList[i];
+            if(!curr.isInteger()){
+                maxDepth = Math.max(maxDepth, calculateMaxDepth(curr.getList()));
+            }
+        }
+        return maxDepth+1;
+    }
+};
