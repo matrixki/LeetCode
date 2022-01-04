4c14063825fe5949e73af577497becdb65b6d626
Mike's Mac Mini <minka1101@gmail.com>
2022-01-03 23:23:07 -0800

    348. Design Tic-Tac-Toe

---
+++ 348. Design Tic-Tac-Toe.js
@@ -0,0 +1,119 @@
+/*
+
+Assume the following rules are for the tic-tac-toe game on an n x n board between two players:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves are allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Implement the TicTacToe class:
+
+TicTacToe(int n) Initializes the object the size of the board n.
+int move(int row, int col, int player) Indicates that the player with id player plays at the cell (row, col) of the board. The move is guaranteed to be a valid move.
+ 
+
+Example 1:
+
+Input
+["TicTacToe", "move", "move", "move", "move", "move", "move", "move"]
+[[3], [0, 0, 1], [0, 2, 2], [2, 2, 1], [1, 1, 2], [2, 0, 1], [1, 0, 2], [2, 1, 1]]
+Output
+[null, 0, 0, 0, 0, 0, 0, 1]
+
+Explanation
+TicTacToe ticTacToe = new TicTacToe(3);
+Assume that player 1 is "X" and player 2 is "O" in the board.
+ticTacToe.move(0, 0, 1); // return 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+ticTacToe.move(0, 2, 2); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+ticTacToe.move(2, 2, 1); // return 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+ticTacToe.move(1, 1, 2); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+ticTacToe.move(2, 0, 1); // return 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+ticTacToe.move(1, 0, 2); // return 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+ticTacToe.move(2, 1, 1); // return 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+ 
+
+Constraints:
+
+2 <= n <= 100
+player is 1 or 2.
+0 <= row, col < n
+(row, col) are unique for each different call to move.
+At most n2 calls will be made to move.
+ 
+
+Follow-up: Could you do better than O(n2) per move() operation?
+
+tags: Amazon, Facebook, Microsoft, Apple, Google
+
+*/
+
+/**
+ * @param {number} n
+ */
+ var TicTacToe = function(n) {
+    this.rows = new Array(n);
+    this.cols = new Array(n);
+    this.diagonal = 0;
+    this.antiDiagonal = 0;
+};
+
+/** 
+ * @param {number} row 
+ * @param {number} col 
+ * @param {number} player
+ * @return {number}
+ */
+TicTacToe.prototype.move = function(row, col, player) {
+    const toAdd = player === 1 ? 1 : -1;
+    this.rows[row] = this.rows[row] ? this.rows[row] + toAdd : toAdd;
+    this.cols[col] = this.cols[col] ? this.cols[col] + toAdd : toAdd;
+    if(row===col){
+        // diagonal
+        this.diagonal+=toAdd;
+    }
+    if(col === (this.cols.length - row -1)){
+        // anti-diagonal
+        this.antiDiagonal+=toAdd;
+    }
+    const len = this.rows.length;
+    if(Math.abs(this.rows[row])===len ||
+       Math.abs(this.cols[col])===len ||
+       Math.abs(this.diagonal) === len ||
+       Math.abs(this.antiDiagonal) === len)
+    {
+        return player;
+    }
+    return 0;
+};
+
+/** 
+ * Your TicTacToe object will be instantiated and called as such:
+ * var obj = new TicTacToe(n)
+ * var param_1 = obj.move(row,col,player)
+ */
