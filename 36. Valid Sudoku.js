58227c5b8f31624f3b5b93dca4d6566fd3faaaaf
Mike's Mac Mini <minka1101@gmail.com>
2022-01-17 00:31:21 -0800

    36. Valid Sudoku

--- 36. Valid Sudoku.js
+++ 36. Valid Sudoku.js
@@ -9,6 +9,7 @@ A partially filled sudoku which is valid.
 Note:
 A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
 
+tags: Amazon, DoorDash, Uber, Apple, Oracle, Cruise Automation, Microsoft, Facebook, Google, Adobe, Snapchat, Intuit
 
 */
 
@@ -16,43 +17,42 @@ A valid Sudoku board (partially filled) is not necessarily solvable. Only the fi
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function(board) {
-    var boxs = [[],[],[],[],[],[],[],[],[]];
-    for( var i=0;i<9;i++ ){
-        var row = [];
-        if( !checkValid( board[i] ) ){
+ var isValidSudoku = function(board) {
+    let box = [[],[],[],[],[],[],[],[],[]];
+    for(let i=0;i<9;i++){
+        let row = board[i], col = [];
+        if(!checkValid(row)){
             return false;
         }
-        for( var j=0;j<9;j++ ){
-            row.push( board[j][i] );
-            var bIndex = 3*Math.floor(i/3) +Math.floor(j/3);
-            boxs[ bIndex ].push( board[i][j] );
+        for(let j=0;j<9;j++){
+            col.push(board[j][i]);
+            const boxIndex = 3*Math.floor(i/3) + Math.floor(j/3);
+            box[boxIndex].push(board[i][j]);
         }
-        if(!checkValid( row )){
+        if(!checkValid(col)){
             return false;
         }
     }
-    for( var i=0;i<9;i++ ){
-        if( !checkValid( boxs[i] ) ){
+    for(let i=0;i<box.length;i++){
+        if(!checkValid(box[i])){
             return false;
         }
     }
     return true;
-    function checkValid(arr){
-        var map = new Set();
-        for( var i=0;i<9;i++ ){
-            if( arr[i] === '.' ){
-                continue;
-            }
-            if( map.has(arr[i]) ){
-                return false;
-            }
-            else{
-                map.add( arr[i] );
-            }
+};
+
+const checkValid = (nums) => {
+    let lookup = new Set();
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]==="."){
+            continue;
         }
-        return true;
+        if(lookup.has(nums[i])){
+            return false;
+        }
+        lookup.add(nums[i]);
     }
+    return true;
 };
 
 //tags: Uber, Apple, Snapchat
