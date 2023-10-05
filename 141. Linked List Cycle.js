fea54404b04d01704e2106c3411ecb3ac2a653eb
Mike's Mac Mini <minka1101@gmail.com>
2023-10-05 23:01:37 +0800

    141. Linked List Cycle

--- 141. Linked List Cycle.js
+++ 141. Linked List Cycle.js
@@ -38,18 +38,23 @@ var hasCycle = function(head) {
     return false;
 };
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
 var hasCycle = function(head) {
-    
-    // slow, fast pointer
-    if( !head || !head.next ){
-        return false;
-    }
-    var slow = head;
-    var fast = head.next;
-    while( slow !== fast ){
-        if( !fast || !fast.next ){
-            return false;
-        }
+    if (!head || !head.next) { return false; }
+    let slow = head, fast = head.next;
+    while (slow !== fast) {
+        if (!fast || !fast.next) { return false; }
         slow = slow.next;
         fast = fast.next.next;
     }
