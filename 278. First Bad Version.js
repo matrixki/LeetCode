721c580f8cbd23b015af576cf048dc9f5d6ab806
Mike's Mac Mini <minka1101@gmail.com>
2023-10-05 23:09:40 +0800

    278. First Bad Version

--- 278. First Bad Version.js
+++ 278. First Bad Version.js
@@ -29,18 +29,18 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        var start = 1, end =n;
-        while( start < end ){
-            var mid = Math.floor((start+end)/2);
-            if(isBadVersion(mid)){
-                end = mid;
-            }
-            else{
-                start = mid+1;
+        let left = 1, right = n;
+        while (left < right) {
+            const mid = left + Math.floor((right-left)/2);
+            const isMidBad = isBadVersion(mid);
+            if (isMidBad) {
+                right = mid;
+            } else {
+                left = mid+1;
             }
         }
-        return start;
+        return left;
     };
 };
 
-//tags: Facebook
+//tags: Facebook, Adobe, Google, Amazon
