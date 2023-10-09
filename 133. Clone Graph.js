6bd79a0609890bace07552450cf2b1e4af85f191
Mike's Mac Mini <minka1101@gmail.com>
2023-10-09 22:26:50 +0800

    133. Clone Graph

--- 133. Clone Graph.js
+++ 133. Clone Graph.js
@@ -24,33 +24,97 @@ Visually, the graph looks like the following:
 
 */
 
+// above is the old version of question content
+
+/*
+
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+
+An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+ 
+
+Example 1:
+
+
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+Example 2:
+
+
+Input: adjList = [[]]
+Output: [[]]
+Explanation: Note that the input contains one empty list. The graph consists of only one node with val = 1 and it does not have any neighbors.
+Example 3:
+
+Input: adjList = []
+Output: []
+Explanation: This an empty graph, it does not have any nodes.
+ 
+
+Constraints:
+
+The number of nodes in the graph is in the range [0, 100].
+1 <= Node.val <= 100
+Node.val is unique for each node.
+There are no repeated edges and no self-loops in the graph.
+The Graph is connected and all nodes can be visited starting from the given node.
+
+*/
+
+
 /**
- * Definition for undirected graph.
- * function UndirectedGraphNode(label) {
- *     this.label = label;
- *     this.neighbors = [];   // Array of UndirectedGraphNode
- * }
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
  */
 
 /**
- * @param {UndirectedGraphNode} graph
- * @return {UndirectedGraphNode}
+ * @param {Node} node
+ * @return {Node}
  */
-var cloneGraph = function(graph) {
-    if (!graph){
-        return graph;
-    }
-    var map = {};
-    return traverse(graph);
-
-    function traverse(node) {
-        if (!map[node.label]) {
-            var newNode = new UndirectedGraphNode(node.label);
-            map[node.label] = newNode;
-            map[node.label].neighbors = node.neighbors.map(traverse);
-        }
-        return map[node.label];
-    }
+var cloneGraph = function(node) {
+    if(!node) {return node;}
+    let visited = [];
+    return traverse(node, visited);
 };
 
-//tags: Google, Facebook, Uber, Pocket Gems
+const traverse = (node, visited) => {
+    const newNode = new Node(node.val);
+    visited[node.val] = newNode;
+    for (let neighbor of node.neighbors) {
+        if (!visited[neighbor.val]) {
+            const newNeighbor = traverse(neighbor, visited);
+            newNode.neighbors.push(newNeighbor);
+        } else {
+            const newNeighbor = visited[neighbor.val];
+            newNode.neighbors.push(newNeighbor);
+        }
+    }
+    return newNode;
+};
+
+//tags: Google, Facebook, Uber, Pocket Gems, Adobe
